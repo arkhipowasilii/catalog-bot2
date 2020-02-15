9852d75137550739de6f1518a85f1d74c6add773
sadsnake <sadsnake@mixbytes.io>
2020-02-15 12:32:37 +0300

    Update catalog

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -1,32 +1,46 @@
-import sqlite3 as sqlite
-from collections import Iterable
 from dataclasses import dataclass
+from typing import Iterable, Optional
 from pathlib import Path
 
+from .db import DatabaseHandler
 
-class DatabaseHandler:
-    def __init__(self, path: Path):
-        assert path.exists()
-        self._connect = sqlite.connect(str(path), check_same_thread=False)
 
 @dataclass
 class Category:
+    id: int
     name: str
 
+
 @dataclass
-class Position:
+class Product:
+    id: int
     name: str
+
 
 class Catalog:
     def __init__(self, path: Path):
         assert path.exists()
         self._db = DatabaseHandler(path)
 
-    def find(self, request: str, offset: int) -> Iterable[Position]:
+    def find(self, request: str, offset: int) -> Iterable[Product]:
         pass
 
-    def get_categories(self, offset: int) -> Iterable[Category]:
-        pass
+    def get_categories(self, offset: Optional[int] = None, limit: Optional[int] = None) -> Iterable[Category]:
+        return [Category(id, name) for id, name in self._db.get_categories(offset=offset, limit=limit)]
 
-    def get(self, category: Category, offset: int) -> Iterable[Position]:
-        pass
+    def get(self, category: Category, offset: Optional[int] = None, limit: Optional[int] = None) -> Iterable[Product]:
+        return [Product(id, name) for id, name in
+            self._db.get_products(category=category.id, offset=offset, limit=limit)]
+
+    def find_products(self, request: str, offset: Optional[int] = None, limit: Optional[int] = None):
+        raw = [(Category(cid, cname), Product(pid, pname)) for cid, cname, pid, pname in self._db.find_products(request, offset, limit)]
+
+        result = {}
+        for category, product in raw:
+            result[category] = result.get(category) or []
+            result[category].append(product)
+
+        return result
+
+    def get_url(self, product: Product) -> Optional[str]:
+        return self._db.get_product_url(product.id)
