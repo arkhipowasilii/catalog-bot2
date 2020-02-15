f21f29407492239ec8c5fc2030437ea266076e8d
sadsnake <sadsnake@mixbytes.io>
2020-02-15 13:01:16 +0300

    Refactor names in catalog class

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Union
 from pathlib import Path
 
 from .db import DatabaseHandler
@@ -22,17 +22,21 @@ class Catalog:
         assert path.exists()
         self._db = DatabaseHandler(path)
 
-    def find(self, request: str, offset: int) -> Iterable[Product]:
-        pass
-
     def get_categories(self, offset: Optional[int] = None, limit: Optional[int] = None) -> Iterable[Category]:
         return [Category(id, name) for id, name in self._db.get_categories(offset=offset, limit=limit)]
 
-    def get(self, category: Category, offset: Optional[int] = None, limit: Optional[int] = None) -> Iterable[Product]:
-        return [Product(id, name) for id, name in
-            self._db.get_products(category=category.id, offset=offset, limit=limit)]
+    def get(self,
+            category: Union[Category, int, None] = None,
+            offset: Optional[int] = None,
+            limit: Optional[int] = None) -> Iterable[Product]:
 
-    def find_products(self, request: str, offset: Optional[int] = None, limit: Optional[int] = None):
+        if isinstance(category, Category):
+            category = category.id
+
+        return [Product(id, name) for id, name in
+            self._db.get_products(category=category, offset=offset, limit=limit)]
+
+    def find(self, request: str, offset: Optional[int] = None, limit: Optional[int] = None):
         raw = [(Category(cid, cname), Product(pid, pname)) for cid, cname, pid, pname in self._db.find_products(request, offset, limit)]
 
         result = {}
