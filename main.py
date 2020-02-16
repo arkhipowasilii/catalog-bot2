1bea550ced9729d382fe11c0900c310233cb140b
sadsnake <sadsnake@mixbytes.io>
2020-02-16 15:43:44 +0300

    Fix typo

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import logging
+from pathlib import Path
 
 from bot import Bot
+from catalog import Catalog
+
 
 def main():
     token = "564367094:AAHofwd0C8yWU68DxcWlGXyT7t1A_o7bDyA"
+    db = Path("./catalog.db")
     logging.basicConfig(level=logging.DEBUG)
-    Bot(token).start()
+    Bot(token, Catalog(db)).start()
 
 if __name__ == '__main__':
     main()
